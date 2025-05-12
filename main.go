74c66d6b51d079e7ff017faf0ded5fed2446aa01
Farid Guzman <ghostrider_x101@msn.com>
2025-05-12 23:26:07 +0200

    for loop, break loop

--- main.go
+++ main.go
@@ -1,48 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "go conf"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
+	bookings := []string{}
 
 	fmt.Printf("welcome to %v booking app \n", conferenceName)
 	fmt.Printf("we have a total of %v tickets and %v still available \n", conferenceTickets, remainingTickets)
 	fmt.Println("get your ticket here")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	var bookings []string
+		fmt.Println("enter first name:")
+		fmt.Scan(&firstName)
 
-	// ask user for name (entr in console)
-	fmt.Println("enter first name:")
-	fmt.Scan(&firstName)
+		fmt.Println("enter last name:")
 
-	fmt.Println("enter last name:")
+		fmt.Scan(&lastName)
 
-	fmt.Scan(&lastName)
+		fmt.Println("enter email:")
+		fmt.Scan(&email)
 
-	fmt.Println("enter email:")
-	fmt.Scan(&email)
+		fmt.Println("enter number of tickets:")
+		fmt.Scan(&userTickets)
 
-	fmt.Println("enter number of tickets:")
-	fmt.Scan(&userTickets)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName + " " + lastName)
+		fmt.Printf("thanks for booking %v %v you booked %v tickets. \n you should be getting an email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-  // fmt.Printf("The whole array: %v\n", bookings)
-  // fmt.Printf("The first value: %v\n", bookings[0])
-  // fmt.Printf("Slice type: %T\n", bookings)
-  // fmt.Printf("Slice length: %v\n", len(bookings))
+		//firstname slice
+		firstNames := []string{}
 
+		// for loop (index number and booking in bookings, look in range on data structure of bookings)
+		// end when iterating iver all bookings elements, _ to define unused variable
 
-	fmt.Printf("thanks for booking %v %v you booked %v tickets. \n you should be getting an email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
-  fmt.Printf("These are all our bookings: %v\n", bookings)
+    noTicketsRemaining := remainingTickets == 0  
+
+    if noTicketsRemaining {
+			// end program
+			fmt.Println("Our conference is booked ut, come back next year")
+			break
+		}
+
+	}
 }
